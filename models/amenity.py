9f34814bcef828032c1f9c2c966a36a000a55bf6
Kitesa <kitesamerga@gmail.com>
2022-10-28 06:03:45 -0700

    Updated amenit

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
+    """Amenity class that inherits from BaseModel and Base"""
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        'Place', secondary='place_amenity', back_populates='amenities')
+    place_amenities = relationship("Place", secondary=place_amenity)
