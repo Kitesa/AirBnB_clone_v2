597ded872468d5c743aa2183b784504500b28c45
Kitesa <kitesamerga@gmail.com>
2022-10-29 03:08:07 -0700

    ADded a script to deploy webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
+@author: Kitesa Merga
 """
 from fabric.api import local, put, run, env
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+env.hosts = ['52.91.133.152', '54.227.137.132']
 
 
 def do_pack():
